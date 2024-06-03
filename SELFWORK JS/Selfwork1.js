abb46a4b1333695c5dac04aa77b0a11625a2e683
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-06-03 16:47:50 +0200

    Add files via upload

---
+++ SELFWORK JS/Selfwork1.js
@@ -0,0 +1,32 @@
+// let num = 2;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${num} x ${i} = ${num * i} `);
+// }
+
+// let num = 3;
+
+// for (let i = 1; i < 10; i++){
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+// let num = 4;
+
+// for(let i = 1; i < 20; i++){
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+
+// let num = 5;
+
+// for(let i = 5; i < 50; i++){
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+
+let num = 6;
+
+for(let i = 1; i < 100; i++){
+    console.log(`${num} x ${i} = ${num * i}`);
+    
+}
